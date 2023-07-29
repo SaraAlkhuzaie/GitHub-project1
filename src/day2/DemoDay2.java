6cb939a5b77bfed6d5052577d09801ae6ba3db7f
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 10:50:03 -0400

    created new method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,9 +5,13 @@ public class DemoDay2 {
 		
 		System.out.println(" Satrday");
 	}
-
+	public static void add( int a,int b) {
+		 System.out.println(a+b);
+	}
 	 public static void subtract( int a,int b) {
 		 System.out.println(a-b);
 		 
 	 }
+	 public static void divid( int a,int b) {
+		 System.out.println(a/b);
 }
