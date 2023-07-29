f41edb432912395c19366b1ce2c81dd73c2444aa
saifA <saifA@Saber.myfiosgateway.com>
2023-07-29 10:34:16 -0400

    implemented  subtract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -3,7 +3,11 @@ package day2;
 public class DemoDay2 {
 	public static void main(String[] args) {
 		
-		System.out.println();
+		System.out.println(" Satrday");
 	}
 
+	 public static void subtract( int a,int b) {
+		 System.out.println(a-b);
+		 
+	 }
 }
