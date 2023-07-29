fa5d3cca759ebfe3fe877abf52474006018e1f89
SaraAlkhuzaie <140735206+SaraAlkhuzaie@users.noreply.github.com>
2023-07-29 10:54:13 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -14,4 +14,7 @@ public class DemoDay2 {
 	 }
 	 public static void divid( int a,int b) {
 		 System.out.println(a/b);
+	 }
+	public static void subtract( int a,int b,int c) {
+		 System.out.println(a-b);
 }
